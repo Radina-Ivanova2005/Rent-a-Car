b23cdd8c3bfa022db53b6610fd59e320ea6146cc
Radina-Ivanova2005 <114239676+Radina-Ivanova2005@users.noreply.github.com>
2024-04-13 23:28:20 +0300

    added YearAttribute.cs

---
+++ YearAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rent_A_Car.Models.Attributes
+{
+    /// <summary>
+    /// Validation attribute to ensure that a year value is not in the future.
+    /// </summary>
+    public class YearAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Determines whether the specified value is a valid year value.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <returns><c>true</c> if the value is a valid year or <c>null</c>; otherwise, <c>false</c>.</returns>
+        public override bool IsValid(object value)
+        {
+            // If the value is null, it's considered valid
+            if (value == null)
+            {
+                return true;
+            }
+
+            // Convert the value to an integer
+            int year = (int)value;
+
+            // Check if the year is less than or equal to the current year
+            return year <= DateTime.Now.Year;
+        }
+    }
+}
