4a278b50a23237c8b30f8bc5b64a36875a123aa3
Hira-eth <127169732+Hira-eth@users.noreply.github.com>
2024-04-14 02:06:54 +0300

    Created User

---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using Rent_A_Car.Models.Attributes;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rent_A_Car.Models
+{
+    public class User : IdentityUser
+    {
+        
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [MinLength(10, ErrorMessage = "EGN must be 10 symbols long.")]
+        [MaxLength(10, ErrorMessage = "EGN must be 10 symbols long.")]
+        [Display(Name = "EGN")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "EGN must contain only numbers.")]
+        [UniqueEGN]
+        public string EGN { get; set; }
+        [UniquePhoneNumber]
+        public string PhoneNumber { get; set; }
+        [UniqueEmail]
+        public string Email { get; set; }
+    }
+}
